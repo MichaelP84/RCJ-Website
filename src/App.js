d8bd54397df5b2ac14be7cc58ac1c0cedfb64a46
ChristopherPasala <christopher.pasala@gmail.com>
2024-03-30 14:52:06 -0500

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import ContactPage from './ContactPage';
 
 function App() {
   return (
     <div className="App">
-     
+      <Routes>
+          <Route path="/" element={ <Home /> }></Route>
+          <Route path="/contact" element= {<ContactPage/>} />
+        </Routes>
     </div>
   );
 }
