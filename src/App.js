bad13f1f5e8a6aa7cd3bb07c3a6a57c6eddbf0c2
Michael R. Pasala <michaelpasala@utexas.edu>
2024-03-30 13:15:44 -0500

    page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     
+      <header className="App-header">
+        <h1>Welcome to RCJ Tech Solutions</h1>
+        <p>Contact us:</p>
+        <p>Email: Paul.Pasala@rcjtechsolutions.om</p>
+        <p>Phone: 713-822-4327</p>
+
+        <h2>Our Services</h2>
+        <h3>Epic Cogito Installations, Support, and Upgrades</h3>
+        <ul>
+          <li>Clarity Upgrades</li>
+          <li>Cogito Caboodle Upgrades</li>
+          <li>SlicerDicer Infrastructure Upgrades</li>
+          <li>Business Objects Software in a Clustered Environment</li>
+          <li>Tableau Enterprise Upgrades</li>
+        </ul>
+
+        <h3>Epic Cogito Solution Architecture</h3>
+        <p>...</p>
+
+        <h3>Cogito Project Management</h3>
+        <p>...</p>
+
+        <h3>Cogito Reporting Tools Expertise</h3>
+        <p>...</p>
+
+        <h3>Tableau Development</h3>
+        <p>...</p>
+
+        <h3>Third Party Data Integration</h3>
+        <p>...</p>
+
+        <h3>Cogito Application Expertise</h3>
+        <p>...</p>
+
+        <h3>Research Analytics</h3>
+        <p>...</p>
+      </header>
     </div>
   );
 }
